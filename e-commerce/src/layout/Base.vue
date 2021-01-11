b14d1cc0d1f4543e664691e47cde3d530308bd95
armandupe <symbolic28@gmail.com>
2021-01-11 16:24:21 +0300

    in progress

--- e-commerce/src/layout/Base.vue
+++ e-commerce/src/layout/Base.vue
@@ -2,19 +2,19 @@
   <div>
     <div>
       <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
-        <router-link class="navbar-brand" to="/">CS-eCommerce</router-link>
+        <router-link class="navbar-brand" to="/">E-Commerce</router-link>
         <div class="ml-auto">
           <div v-if="user.photoURL">
             <img
               :src="user.photoURL"
               class="img-thumbnail profile-image"
-              alt
+              alt=""
             />
             <router-link class="btn btn-primary my-2 my-sm-0" to="/cart">
               <img
                 src="https://pngimg.com/uploads/shopping_cart/shopping_cart_PNG38.png"
                 width="50"
-                alt
+                alt=""
               />
               <span class="badge badge-danger badge-pill">{{ cart.length }}</span>
             </router-link>
@@ -40,4 +40,34 @@ export default {
 </script>
 
 <style>
+nav {
+  background-color: teal;
+}
+.navbar-brand {
+  font-weight: bold;
+  font-size: 25px;
+  color: #ffffff !important;
+}
+.profile-image {
+  width: 50px;
+  border-radius: 100% !important;
+}
+.page-container {
+  padding-top: 81px;
+}
+.btn {
+  border-radius: 0%;
+  font-weight: bold;
+  background: teal;
+  border: teal;
+}
+.btn:hover {
+  background: #00b4b4;
+}
+input {
+  border-radius: 0%;
+}
+.btn:focus {
+  background: teal;
+}
 </style>
